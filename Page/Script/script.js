8ed96b4f8149c6f001b713fdea8eeaa2ff94dd76
Patrik Bågenholm <sputtrosa@gmail.com>
2019-03-28 14:40:17 +0100

    Add answer colors

--- Page/Script/script.js
+++ Page/Script/script.js
@@ -1,20 +1,12 @@
-//TODO: Result table
-//TODO: Settings page
-
 var basicApiAdress = 'https://opentdb.com/api.php?amount=10&type=multiple';
 var questionArray;
 var nextQuestionIndex = 0;
 var alternatives;
 var points = 0;
 var questionsAsked = 0;
+var questionState = true;
 
 getQuestions();
-
-function checkSettings() {
-    let category = document.getElementById("category");
-    let difficulty = document.getElementById("difficulty");
-    return basicApiAdress + '&category=' + category.value + '&difficulty=' + difficulty.value;
-}
 
 function getQuestions() {
     var url = checkSettings();
@@ -27,13 +19,19 @@ function getQuestions() {
     });
 }
 
+function checkSettings() {
+    let category = document.getElementById("category");
+    let difficulty = document.getElementById("difficulty");
+    return basicApiAdress + '&category=' + category.value + '&difficulty=' + difficulty.value;
+}
+
 function newQuestion() {
     alternatives = makeAlternativeArray().sort();
     document.getElementById("question").innerHTML = questionArray.results[nextQuestionIndex].question;
-    document.getElementById("alt1").innerHTML = alternatives[0];
-    document.getElementById("alt2").innerHTML = alternatives[1];
-    document.getElementById("alt3").innerHTML = alternatives[2];
-    document.getElementById("alt4").innerHTML = alternatives[3];
+    document.getElementById("alt0text").innerHTML = alternatives[0];
+    document.getElementById("alt1text").innerHTML = alternatives[1];
+    document.getElementById("alt2text").innerHTML = alternatives[2];
+    document.getElementById("alt3text").innerHTML = alternatives[3];
 }
 
 function makeAlternativeArray() {
@@ -42,16 +40,29 @@ function makeAlternativeArray() {
 }
 
 function selectAnswer(choice) {
-    questionsAsked++;
-    document.getElementById("currentQuiz").value = nextQuestionIndex + 1;
-    if (alternatives[choice] == questionArray.results[nextQuestionIndex].correct_answer) {
-        points++;
-        document.getElementById("answer").innerHTML = "Correct!";
+    if (questionState) {
+       // setAlternativeColors();
+        document.getElementById("currentQuiz").value = nextQuestionIndex + 1;
+        setAnswerColors();
+        if (alternatives[choice] == questionArray.results[nextQuestionIndex].correct_answer) {
+            points++;
+            document.getElementById("answer").innerHTML = "Correct!";
+        } else {
+            document.getElementById("answer").innerHTML = "Incorrect! The right answer was " + questionArray.results[nextQuestionIndex].correct_answer;
+        }
+        questionsAsked++;
+        document.getElementById("result").innerHTML = "" + points + "/" + questionsAsked;
+        questionState = false;
     } else {
-        document.getElementById("answer").innerHTML = "Incorrect! The right answer was " + questionArray.results[nextQuestionIndex].correct_answer;
+        document.getElementById("result").innerHTML = "" + points + "/" + questionsAsked;
+        nextQuestionIndex++;
+        checkQuestionIndex();
+        setAlternativeColors();
+        questionState = true;
     }
-    document.getElementById("result").innerHTML = "" + points + "/" + questionsAsked;
-    nextQuestionIndex++;
+}
+
+function checkQuestionIndex() {
     if (nextQuestionIndex < questionArray.results.length) {
         newQuestion();
         checkSettings();
@@ -60,5 +71,23 @@ function selectAnswer(choice) {
         nextQuestionIndex = 0;
         getQuestions();
         console.log("Out of questions! Your total was " + points)
+    }
+}
+
+function setAlternativeColors() {
+    elements = document.getElementsByClassName("altbox");
+    for (let i = 0; i < elements.length ; i++) {
+        elements[i].style.backgroundColor = "#444";
+    }
+}
+
+function setAnswerColors(){
+    elements = document.getElementsByClassName("altbox");
+    for (let i = 0; i < elements.length; i++) {
+        if (alternatives[i] == questionArray.results[nextQuestionIndex].correct_answer) {
+            elements[i].style.backgroundColor = "green";
+        } else {
+            elements[i].style.backgroundColor = "red"
+        }
     }
 }
